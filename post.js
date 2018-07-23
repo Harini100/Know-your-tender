b03ab3fe00ec9fea488a4cf9bf3707a82f0a590e
Harini100 <hariniks19@gmail.com>
2018-07-23 19:27:43 +0530

    Add files via upload

---
+++ post.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var multer  =   require('multer');
+var mime    =   require('mime');
+var mysql = require('mysql');
+var app = express();
+var bodyParser =    require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/angulartest.html', function(req, res){
+    res.sendFile(__dirname + '/' + 'angulartest.html');
+    console.log("----------------");
+});
+app.post("/post", function (req, res) {
+	    var response2;
+		var value1=req.body.cName;
+		var value2=req.body.aName;
+		var value3=req.body.tName
+
+/*		 response1 = {
+      id:"test1",
+      av:"test2"
+   };
+   console.log(response1);
+   console.log(JSON.stringify(response1));*/
+   var request = require('request');
+request('http://localhost:6000', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+	  response2 = body ;
+	  console.log("TEST:CITY NAME IS  "+value1);
+		console.log("TEST:AREA NAME IS  " +value2);
+		console.log("TEST:TENDER TYPE IS  " +value3);
+    console.log(body); // Print the body of response.
+	res.end(body);
+  }
+})
+   //console.log("hi" + response2);
+   //res.end(JSON.stringify(response2));
+  
+});
+app.listen(3000);
